a34078d98ab9d41cea290e34b395d7701758def6
Reid Miller <reid.s.miller@gmail.com>
2023-03-17 20:09:10 -0600

    feat: Add a bunch of stuff

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = ['Task 1', 'Task 2', 'Task 3']
+    @tasks = Task.all
   end
 
   def new
@@ -11,9 +11,12 @@ class TasksController < ApplicationController
       title: params[:task][:title],
       description: params[:task][:description]
     })
-
     task.save
-
     redirect_to '/tasks'
+  end
+
+  def show
+    require 'pry'; binding.pry
+    @task = Task.find(params[:id])
   end
 end
